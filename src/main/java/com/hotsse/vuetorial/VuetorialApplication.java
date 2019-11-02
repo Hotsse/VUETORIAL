a8e2d9f6642d587f2343ac50e6dd4ef26e8a2bb5
Hotsse <dkdlrja@naver.com>
2019-11-02 20:46:02 +0900

    project initialize

---
+++ src/main/java/com/hotsse/vuetorial/VuetorialApplication.java
@@ -0,0 +1,13 @@
+package com.hotsse.vuetorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuetorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuetorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotsse/vuetorial/VuetorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotsse.vuetorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuetorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
